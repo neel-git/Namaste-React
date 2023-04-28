cc6c94f359f7db63457f6cb23dcdaf97c265e833
Neelkamal <neelkamalsinghthegisb@gmail.com>
2023-04-29 01:29:30 +0530

    Episode-1

---
+++ app.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement(
+//     "h1",
+//     {id: "heading"},
+//     "Hello World from React !!"
+//   );
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h2", {}, "I'm a h2 tag"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
